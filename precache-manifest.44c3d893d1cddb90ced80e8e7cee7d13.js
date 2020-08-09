886d6464710c90aabfa6a1303e20da5bb6f6e23b
akashannex <akash@annexlogics.com>
2020-08-09 18:40:09 +0530

    Updates

---
+++ precache-manifest.44c3d893d1cddb90ced80e8e7cee7d13.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b9fc853c1625f578eacd24a0afc423",
+    "url": "/EatingApp/index.html"
+  },
+  {
+    "revision": "ad1a5ba8d51e917f02c6",
+    "url": "/EatingApp/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "965651fb15f75833027c",
+    "url": "/EatingApp/static/css/main.059a5037.chunk.css"
+  },
+  {
+    "revision": "ad1a5ba8d51e917f02c6",
+    "url": "/EatingApp/static/js/2.268b5c2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/EatingApp/static/js/2.268b5c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965651fb15f75833027c",
+    "url": "/EatingApp/static/js/main.4dd0d0e3.chunk.js"
+  },
+  {
+    "revision": "5a7990e56c3dff105b57",
+    "url": "/EatingApp/static/js/runtime-main.cf54b21e.js"
+  },
+  {
+    "revision": "f2a5883d48433483b73fe94f6e458429",
+    "url": "/EatingApp/static/media/1510305276-8424-removebg-preview.f2a5883d.png"
+  },
+  {
+    "revision": "4064ba306b82a6c209ef736b9f7a6495",
+    "url": "/EatingApp/static/media/2933521.4064ba30.png"
+  },
+  {
+    "revision": "a6dbb9f99dbb6f33e8252a7e8cb56497",
+    "url": "/EatingApp/static/media/753066.a6dbb9f9.png"
+  },
+  {
+    "revision": "9a74335ce9334345a72f199f2d997513",
+    "url": "/EatingApp/static/media/Six-pack-abs-removebg-preview.9a74335c.png"
+  },
+  {
+    "revision": "212a54e232a8c95cbb9ec75d30fae0d1",
+    "url": "/EatingApp/static/media/basic-male-img-1.212a54e2.png"
+  },
+  {
+    "revision": "d0899f4351b0d5815f15c232163c7312",
+    "url": "/EatingApp/static/media/healthy-lifestyle-fitness-girl_98292-5118-removebg-preview.d0899f43.png"
+  },
+  {
+    "revision": "5d48488e830a514769e9d6791d9298df",
+    "url": "/EatingApp/static/media/jogging-28-1139596.5d48488e.png"
+  },
+  {
+    "revision": "a5ed9d13c5bd6d8dee1c7f4f65e242d2",
+    "url": "/EatingApp/static/media/png-transparent-running-running-man-physical-fitness-hand-sport-thumbnail-removebg-preview.a5ed9d13.png"
+  },
+  {
+    "revision": "5f4554ca486e172e26d85f64ad94195d",
+    "url": "/EatingApp/static/media/woman-measuring-her-waist-illustration_53876-43294-removebg-preview.5f4554ca.png"
+  },
+  {
+    "revision": "d402a4bec5eb622fb616d33e72201428",
+    "url": "/EatingApp/static/media/woman-with-measuring-tape_144627-28905-removebg-preview.d402a4be.png"
+  },
+  {
+    "revision": "f80a6b476f451d6f8cfc8a834988f811",
+    "url": "/EatingApp/static/media/young-sexy-woman-fitness_98292-5116-removebg-preview.f80a6b47.png"
+  }
+]);
